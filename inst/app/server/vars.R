eb2cf0ce3777725e4c9d7d726933eae914e759ad
Jack Olney <jack.olney@me.com>
2017-02-06 17:22:37 +0000

    var.R for collecting per-session variables

---
+++ inst/app/server/vars.R
@@ -0,0 +1,54 @@
+## Blank variables that replace deprecated globals
+# Need to know the type of each to initialise variables
+
+# Start by eliminating 'MasterData' from .GlobalEnv then do the rest
+# remember that I do a lot of rm(MasterData) crap so I need to re-write this too.
+# Just reset it to MasterData <- list()
+
+## Muy Importante
+MasterData <- list()
+
+# Need to look for:
+# rm(MasterData)
+# exists("MasterData") too
+
+## Calibration
+# CalibIncOut
+# CalibInitOut
+# CalibOut
+# CalibParamOut
+# countryReportName
+# countryReportName
+# minError
+# minErrorRun
+# ParamMaxMin
+# runError
+# selectedRuns
+
+## Optimisation
+# baseTenPercentCalibInitial
+# shuffledRuns
+# optResults
+# BaselineAdhr
+# BaselineCost
+# BaselineInit
+# BaselineLink
+# BaselinePreR
+# BaselineRetn
+# BaselineTest
+
+## Report
+# report_909090_adherence
+# report_909090_adherence_BASE
+# report_909090_cost
+# report_909090_cost_BASE
+# report_909090_initiation
+# report_909090_initiation_BASE
+# report_909090_linkage
+# report_909090_linkage_BASE
+# report_909090_preRetention
+# report_909090_preRetention_BASE
+# report_909090_retention
+# report_909090_retention_BASE
+# report_909090_testing
+# report_909090_testing_BASE
